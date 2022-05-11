db62291c68ff42de2d586de212738bd1fcf34f4f
strbytes <strbytes@gmail.com>
2022-05-11 17:12:01 -0400

    Add Black formatter

--- exercises/merge_sort.py
+++ exercises/merge_sort.py
@@ -1,11 +1,14 @@
 def merge_sort(lst):
     if len(lst) == 1:
         return lst
-    return merge(merge_sort(lst[:len(lst)//2]), merge_sort(lst[len(lst)//2:]))
+    return merge(merge_sort(lst[: len(lst) // 2]), merge_sort(lst[len(lst) // 2 :]))
+
 
 def merge(a, b):
-    if not a: return b
-    elif not b: return a
+    if not a:
+        return b
+    elif not b:
+        return a
     i, j = 0, 0
     solution = []
     while i < len(a) and j < len(b):
@@ -15,11 +18,14 @@ def merge(a, b):
         else:
             solution.append(b[j])
             j += 1
-    if a[i:]: solution.extend(a[i:])
-    elif b[j:]: solution.extend(b[j:])
+    if a[i:]:
+        solution.extend(a[i:])
+    elif b[j:]:
+        solution.extend(b[j:])
     return solution
+
 
 if __name__ == "__main__":
     import random
-    print(merge_sort([random.randint(0, 99) for i in range(100)]))
 
+    print(merge_sort([random.randint(0, 99) for i in range(100)]))

08a41572eb2739cf180b12c96909801d7b12ac83
strbytes <strbytes@gmail.com>
2022-05-12 14:54:59 -0400

    Add getter and setter methods to Link
    Added ability to construct a populated List by calling the constructor
    with *args, insert and pop methods, and __delitem__, __getitem__, and
    __setitem__ methods. Also tweaked string representations and fixed a bug
    where _traverse was not identifying IndexErrors correctly.

--- exercises/linked_list.py
+++ exercises/linked_list.py
@@ -13,10 +13,12 @@ class Node:
         return 1 + len(self.rest)
 
     def _traverse(self, i):
+        if isinstance(i, slice):
+            raise NotImplementedError("slices not implemented for Link/Node")
         if i < 0:
             l = len(self)
             i = l + i
-            if i > l:
+            if i < 0:
                 raise IndexError("Node index out of range")
         if self.rest == Node.empty and i > 0:
             raise IndexError("Node index out of range")
@@ -24,7 +26,7 @@ class Node:
             return self
         return Node._traverse(self.rest, i - 1)
 
-    def __str__(self, new=True, start="(", delimit=" ", declose="", end=")"):
+    def __str__(self, new=True, start="(", delimit=", ", declose="", end=")"):
         if self is Node.empty:
             return end
         if isinstance(self.first, Node):
@@ -37,7 +39,7 @@ class Node:
                 end=end,
             )
         else:
-            value = str(self.first)
+            value = repr(self.first)
         use_start = start if new else ""
         use_delimit = delimit if self.rest else ""
         use_declose = declose if self.rest else ""
@@ -61,13 +63,15 @@ class Node:
 
 
 class Link:
-    def __init__(self):
+    def __init__(self, *args):
         self.head = Node.empty
+        for item in reversed(args):
+            self.head = Node(item, self.head)
 
-    def __len__(self):
-        if self.head is Node.empty:
-            return 0
-        return len(self.head)
+    def insert(self, i, v):
+        l = self.head._traverse(i)
+        l.rest = Node(l.first, l.rest)
+        l.first = v
 
     def append(self, value):
         if self.head is Node.empty:
@@ -79,14 +83,46 @@ class Link:
     def prepend(self, value):
         self.head = Node(value, self.head)
 
+    def pop(self):
+        if self.head == Node.empty:
+            raise IndexError("pop from empty list")
+        if len(self) == 1:
+            v = self.head.first
+            self.head = Node.empty
+            return v
+        l = self.head._traverse(-2)
+        v = l.rest.first
+        l.rest = Node.empty
+        return v
+
+    def __delitem__(self, i):
+        if i >= len(self):  # Check for index 0 on empty list or i == len(self)
+            raise IndexError("Link index out of range")
+        elif i == 0:
+            self.head = self.head.rest
+        else:
+            l = self.head._traverse(i - 1)
+            l.rest = l.rest.rest
+
+    def __getitem__(self, i):
+        l = self.head._traverse(i)
+        return l.first
+
+    def __setitem__(self, i, v):
+        l = self.head._traverse(i)
+        l.first = v
+
+    def __len__(self):
+        if self.head is Node.empty:
+            return 0
+        return len(self.head)
+
     def __str__(self):
         return str(self.head)
 
     def __repr__(self):
-        return self.head.__repr__()
+        return "Link" + str(self)
 
 
 if __name__ == "__main__":
-    a = Link()
-    for i in range(3):
-        a.append(i)
+    a = Link(1, 2, 3)

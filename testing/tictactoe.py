8399da0dbf1ccbbad3a7a98849bb9c4d041b3841
strbytes <strbytes@gmail.com>
2022-05-15 17:59:23 -0400

    Add method to display available moves

--- testing/tictactoe.py
+++ testing/tictactoe.py
@@ -10,3 +10,13 @@ class Board:
             + "-----------\n"
             + f" {self.plays[6]} | {self.plays[7]} | {self.plays[8]} "
         )
+
+    def available_moves(self):
+        available = [i + 1 if self.plays[i] == "." else self.plays[i] for i in range(9)]
+        return (
+            f" {available[0]} | {available[1]} | {available[2]} \n"
+            + "-----------\n"
+            + f" {available[3]} | {available[4]} | {available[5]} \n"
+            + "-----------\n"
+            + f" {available[6]} | {available[7]} | {available[8]} "
+        )
